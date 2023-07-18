2df9b5b55656d3f3ff9a611b2e0592999e2e7cd8
Refi Ahmad Fauzi <refiahmad05@gmail.com>
2023-07-18 06:10:37 +0000

    responsive

---
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,13 @@
+export default {
+	
+	login () {
+		var iniemail = email.text;
+		var inipassword = password.text;
+		
+	},
+	async myFun2 () {
+		Image3.setVisibility(false)
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
